79144019cca687c7155e084ff7bbea456d70f5d2
Elisha Kyakopo <53124548+elisha2kyakpo1@users.noreply.github.com>
2021-03-09 15:10:36 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :followeds, through: :active_followings, foreign_key: 'followed_id'
   has_many :followers, through: :passive_followings, foreign_key: 'follower_id'
 
-  has_one_attached :Photo
+  has_one_attached :photo
   has_one_attached :cover_image
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :full_name, presence: true, length: { minimum: 4, maximum: 50 }
